44a31291e6ec6756f828e5bf927f1f535ee6d556
jaten16 <jatenliu16@gmail.com>
2026-03-23 18:20:00 +0800

    feat : initial interface contract including  4 interfaces

---
+++ shared/contract.ts
@@ -0,0 +1,29 @@
+// shared/contract.ts
+
+//用户信息接口
+export interface User {
+    id : number;         //用户id
+    username : string;  //用户名称
+    timeStamp : number;   //账户创建时间
+}
+
+//知识节点接口
+export interface CWFrameNode {
+    id : number;                //节点id
+    label : string;             //节点标签/标题
+    description : string;       //节点描述    
+    dependencies : number[];    //依赖节点 ===> 理解为图中的边  
+    // 比如A.dependencies = [B, C] 表示 A 依赖 B 和 C。就是B -> A、C -> A
+}
+
+//知识框架图接口
+export interface CWFrameMap {
+    version : string | number;  //框架版本
+    nodes : CWFrameNode[];      //节点数组    nodes 是图里的顶点集合。
+}
+
+//用户进度接口
+export interface CWFrameProgress {
+    userId : number;
+    unlockedNodes: Record<number, { unlockedAt: number }>; // 点亮记录
+}
