26003cc465918fd89f0a340749e621d78840f5a3
jaten16 <jatenliu16@gmail.com>
2026-03-23 20:55:43 +0800

    feat : initial frontend and backend  code frame

---
+++ frontend/src/App.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+// import { mockNodes } from './mock/mockData'; // 稍后创建这个文件
+import type { CWFrameNode } from '@shared/contract';
+
+console.log("Contract linked!");
+</script>
