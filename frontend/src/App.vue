d1bb051588f1750c1ad051f55f6db71b78d53b91
ChaoXi Liu <jatenliu16@gmail.com>
2026-04-01 16:03:53 +0800

    chores : add code comments of Application entry layer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,12 @@
 <template>
+  <!-- 这个cwframe-app的div是整个应用的根容器，它占据了整个页面的宽度高度，并且没有外边距和内边距，溢出隐藏，背景是var(--bg) -->
   <div id="cwframe-app">
-    <!-- Main Router View with transitions for smooth experience -->
-    <router-view v-slot="{ Component }">
+    <!-- 这个router-view是用来渲染路由视图的，它会在页面中显示当前路由的组件 -->
+    <router-view v-slot="{ Component }">  <!-- Component是一个具名插槽，它会在页面中显示当前路由的组件 -->
+      <!-- 这个transition是用来实现页面切换的过渡效果的，它会在页面切换时使用淡入淡出的效果 -->
       <transition mode="out-in">
-        <component :is="Component" />
+        <!-- 这个component是用来渲染当前路由的组件的，它会在页面中显示当前路由的组件 -->
+        <component :is="Component" />   <!-- 这里的Component就是当前路由的组件 -->
       </transition>
     </router-view>
   </div>
