3784d80b68d15b1229041ac9bde9d65f9318f7d1
ChaoXi Liu <jatenliu16@gmail.com>
2026-04-11 12:25:29 +0800

    refactor: remove legacy node api route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,7 +3,6 @@ import cors from 'cors';
 import authRouter from './routers/auth.router';
 import progressRouter from './routers/progress.router';
 import mapRouter from './routers/map.router';
-import nodeRouter from './routers/node.router';
 import { notFoundMiddleware } from './middleware/not-found.middleware';
 import { errorMiddleware } from './middleware/error.middleware';
 import { authMiddleware } from './middleware/auth.middleware';
@@ -16,7 +15,6 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/auth', authRouter);
 app.use('/api/maps', mapRouter);
-app.use('/api/nodes', nodeRouter);
 app.use('/api/users', authMiddleware, progressRouter);
 
 app.use(notFoundMiddleware);
