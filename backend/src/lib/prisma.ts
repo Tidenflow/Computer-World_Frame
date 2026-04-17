f73e8c9a15db721d922663526dc81f37a5332cf9
ChaoXi Liu <jatenliu16@gmail.com>
2026-04-17 21:13:35 +0800

    refactor(pre): delete frontend and backend

--- backend/src/lib/prisma.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-import { config } from '../config';
-
-export const prisma: PrismaClient = new PrismaClient({
-  log: ['error', 'warn'],
-  datasourceUrl: config.db.databaseUrl,
-});
